8e78f62e386c35455115f17eff623a7f9063aa36
SergBogoduhov <SBogoduhovv@yandex.ru>
2023-03-26 23:05:01 +0300

    DZ3

---
+++ bin/Debug/net6.0/DZ3/Program.cs
@@ -0,0 +1,13 @@
+﻿string A = Console.ReadLine();
+
+int numA = Convert.ToInt32(A);
+
+if (numA % 2 == 0){
+    
+    Console.WriteLine("Число является четным");
+}
+else if (numA % 2 == 1){
+
+
+    Console.WriteLine("Число является нечетным");
+}
