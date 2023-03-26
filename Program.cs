7648459b53c52de68716335842bb78015bdb6dd2
SergBogoduhov <SBogoduhovv@yandex.ru>
2023-03-26 20:21:07 +0300

    Сщхранение первого задания

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string A = Console.ReadLine();
+string B = Console.ReadLine();
+
+int numA = Convert.ToInt32(A);
+int numB = Convert.ToInt32(B);
+
+if (numA > numB)
+{
+    Console.WriteLine(numA);
+}
+else
+{
+    Console.WriteLine(numB);
+}
