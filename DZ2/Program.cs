9c4c4b0f137513d0f17c427f0407dd568ff7b9f5
SergBogoduhov <SBogoduhovv@yandex.ru>
2023-03-26 20:59:57 +0300

    Недоделанное второе задание

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿string A = Console.ReadLine();
+string B = Console.ReadLine();
+string С = Console.ReadLine();
+
+int numA = Convert.ToInt32(A);
+int numB = Convert.ToInt32(B);
+int numС = Convert.ToInt32(С);
+
+if (numA > numB)
+{
+    if (numA > numC){
+        Console.WriteLine(numA);
+     }
+    else Console.WriteLine(numC);
+
+}
+else if (numB > numC)
+{
+    Console.WriteLine(numB);
+}
+else{
+    Console.WriteLine(numC);
+}
