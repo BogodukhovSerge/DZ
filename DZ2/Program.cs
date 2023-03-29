a8ab57400d366f75d4f4699382eea3d12027e93d
SergBogoduhov <SBogoduhovv@yandex.ru>
2023-03-29 15:09:00 +0300

    Сохранение после трнтьего дз2 последнее

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -4,6 +4,12 @@ string X = Console.ReadLine();
 
 int numA = Convert.ToInt32(A);
 int numB = Convert.ToInt32(B);
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
 int numX = Convert.ToInt32(X);
 
 if (numA > numB)
