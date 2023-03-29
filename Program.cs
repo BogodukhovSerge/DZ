a6ebad6b7d28fbb9142e91b464932d20a738a386
SergBogoduhov <SBogoduhovv@yandex.ru>
2023-03-29 15:07:14 +0300

    Сохранение после трнтьего дз1 последнее

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿string A = Console.ReadLine();
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+string A = Console.ReadLine();
 string B = Console.ReadLine();
 
 int numA = Convert.ToInt32(A);
